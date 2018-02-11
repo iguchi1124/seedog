193c06e7e2c9986e4baa162c90e0a26504a44540
Shota Iguchi <shota-iguchi@cookpad.com>
2018-02-11 21:07:25 +0900

    Support active model attributes

--- lib/seedog/dsl/record.rb
+++ lib/seedog/dsl/record.rb
@@ -5,27 +5,26 @@ module Seedog
         @evaluator = evaluator
         @model = model
         @upsert_attributes = upsert_attributes
+        @attrs = {}.with_indifferent_access
+      end
+
+      def method_missing(method, arg)
+        @attrs[method] = arg
       end
 
       def call(&block)
-        attrs = {}
-        @model.model_class.attribute_names.each do |attr|
-          define_singleton_method(attr) do |value|
-            attrs[attr] = value
-          end
-        end
-
         instance_exec(&block) if block
 
-        before_attrs = instance.attributes.slice(*attrs.keys)
+        before_attrs = instance.attributes.with_indifferent_access.slice(*@attrs.keys)
+
         if instance.new_record?
-          puts "\33[32mCreate #{@model.model_class.name.underscore} #{@upsert_attributes.merge(attrs)}\33[0m"
-        elsif before_attrs != attrs
-          puts "Update #{@model.model_class.name.underscore} #{@upsert_attributes.merge(before_attrs)} to #{@upsert_attributes.merge(attrs)}"
+          puts "\33[32mCreate #{@model.model_class.name.underscore} #{@upsert_attributes.merge(@attrs)}\33[0m"
+        elsif before_attrs != @attrs
+          puts "Update #{@model.model_class.name.underscore} #{@upsert_attributes.merge(before_attrs)} to #{@upsert_attributes.merge(@attrs)}"
         end
 
         unless @evaluator.dry_run
-          instance.assign_attributes(attrs)
+          instance.assign_attributes(@attrs)
           instance.save!(validate: @evaluator.config.validate)
         end
       end
