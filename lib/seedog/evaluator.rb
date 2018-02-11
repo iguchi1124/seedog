f94e97b7a7a9a45d9c5904340fc8b82778b7ca75
Shota Iguchi <shota-iguchi@cookpad.com>
2018-02-11 21:15:43 +0900

    Add log of apply schemafile path

--- lib/seedog/evaluator.rb
+++ lib/seedog/evaluator.rb
@@ -10,6 +10,12 @@ module Seedog
     end
 
     def run
+      if @dry_run
+        puts "Apply `#{config.file.relative_path_from(Rails.root)}` (dry-run)"
+      else
+        puts "Apply `#{config.file.relative_path_from(Rails.root)}`"
+      end
+
       dsl = DSL.new(self)
       dsl.instance_eval(config.file.read)
     end
